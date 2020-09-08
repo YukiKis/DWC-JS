8ea036ccf81b71fdcbf781c518b1908e7f860097
Yuki-K <s143068@gmail.com>
2020-09-08 21:37:03 +0900

    [FINISH] JS-3

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
-var i;
-var num = 0;
-
-for (i = 1; i < 11; i++) {
-  num += i;
+function getJsHand() {
+  var js_hand_num = Math.floor(Math.random() * 3);
+  var hand;
+  if (js_hand_num == 0) {
+    hand = "グー";
+  } else if (js_hand_num == 1) {
+    hand = "チョキ";
+  } else {
+    hand = "パー";
+  }
+  return hand;
 }
 
-alert("1から10まで足し算した結果は" + num + "です");
+function winLose(user, js) {
+  var winLose;
+  if (user == "グー") {
+    if (js == "グー") {
+      winLose = "あいこ";
+    } else if (js == "チョキ") {
+      winLose = "勝ち";
+    } else {
+      winLose = "負け";
+    }
+  } else if (user == "チョキ") {
+    if (js == "グー") {
+      winLose = "負け";
+    } else if (js == "チョキ") {
+      winLose = "あいこ";
+    } else {
+      winLose = "勝ち";
+    }
+  } else {
+    if (js == "グー") {
+      winLose = "勝ち";
+    } else if (js == "チョキ") {
+      winLose = "負け";
+    } else {
+      winLose = "あいこ";
+    }
+  }
+  return winLose;
+}
+
+var user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください");
+while (
+  user_hand != "グー" &&
+  user_hand != "チョキ" &&
+  user_hand != "パー" &&
+  user_hand != null
+) {
+  alert("グー・チョキ・パーのいずれかを入力してください");
+  user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください");
+}
+if (user_hand == null) {
+  alert("またチャレンジしてね");
+} else {
+  var js_hand = getJsHand();
+  var judge = winLose(user_hand, js_hand);
+  alert(judge);
+}
